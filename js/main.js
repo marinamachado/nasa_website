e04c7f5b9a94cb2ed5c15f76237dcb9f461f0e1b
Marina Fontes Alcântara Machado <39441826+marinamachado@users.noreply.github.com>
2020-05-31 02:33:33 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-// (function($) {
-//   "use strict";
-//   $(window).on("load", function() { // makes sure the whole site is loaded
-//     //preloader
-//     $("#status").fadeOut(); // will first fade out the loading animation
-//     $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
+(function($) {
+  "use strict";
+  $(window).on("load", function() { // makes sure the whole site is loaded
+    //preloader
+    $("#status").fadeOut(); // will first fade out the loading animation
+    $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
     
-//     //masonry
-//     $('.grid').masonry({
-//       itemSelector: '.grid-item'
+    //masonry
+    $('.grid').masonry({
+      itemSelector: '.grid-item'
       
-//     });    
-//   });
+    });    
+  });
 
 
   $(document).ready(function(){  
